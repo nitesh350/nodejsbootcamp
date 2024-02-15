6bab1ba4c9586250a4e476198c9b09f4b3435ab1
nitesh350 <niteshshrestha4321@gmail.com>
2024-02-15 23:21:28 +0545

    text sync and sended to empty.text file

--- day1/index.js
+++ day1/index.js
@@ -1,3 +1,6 @@
 const fs = require('fs');
 const show = fs.readFileSync('./node-farm/read.text', 'utf-8');
 console.log(show)
+
+const textout = `What i want to be in future is : ${show}.\n created on ${Date.now()}`;
+fs.writeFileSync('./node-farm/empty.text',textout);
